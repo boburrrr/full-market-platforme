a815a54a1a0ed501535b5c0c58711c8dc93e6d6e
bobur <davlatboyev2023@gmail.com>
2023-09-20 19:01:47 -0700

    Initial commit

---
+++ src/main/java/com/example/fullmarketplatforme/FullMarketPlatformeApplication.java
@@ -0,0 +1,13 @@
+package com.example.fullmarketplatforme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullMarketPlatformeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FullMarketPlatformeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/fullmarketplatforme/FullMarketPlatformeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fullmarketplatforme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullMarketPlatformeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
